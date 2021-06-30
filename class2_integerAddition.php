2ec39d6807ace580291c4b50487fffdd621f94c7
Ajit Patil <54498483+ajitpatil102001@users.noreply.github.com>
2021-06-30 11:36:34 +0530

    Add files via upload

---
+++ class2_integerAddition.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>class2</title>
+</head>
+<body>
+   <?php
+   $x=10;
+   $y=20;
+   $z=$x+$y;
+   //echo $z;
+  echo"Total is= $z";
+   ?> 
+</body>
+</html>
---
+++ class2moredatatype.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../PhpProject/style.css">
+</head>
+<body>
+
+   <?php
+  
+   $name="AJIT";
+   $class="12th";
+   $physics=78;
+   $maths=71;
+   $science=98;
+   $total=$physics+$maths+$science;
+   echo"Student Name=$name<br>";
+   echo"Class=$class<br>";
+   echo"Total Marks=$total<br>";
+   
+   ?> 
+ 
+</body>
+</html>
