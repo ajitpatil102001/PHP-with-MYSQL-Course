4b3722cf85d2c562d2c383c8b34313e5a3e912a5
Ajit Patil <54498483+ajitpatil102001@users.noreply.github.com>
2021-06-30 15:37:29 +0530

    Add files via upload

---
+++ class4_operators.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+   <?php
+  /* //arithmatic operators:
+// Apko yeh pata honge na bro
+// addition substraction mul and div
+$a=12;
+$b=8;
+$c=$a%$b;
+$d=$a**$b;
+echo "MOdulas is =$c <br>";
+//exponatiation
+echo "exponatiation is=$d";
+//assignment oprator
+$x=20;
+$y=20;
+$y+=$x;
+echo"y value=$y <br>";
+//comparison operator
+$x=20;
+$y=20;
+if($x==$y){
+echo("This is true");
+}*/
+//increment operator
+$x=20;
+$y=30;
+$m=++$x;
+echo" $m";
+   ?> 
+   
+</body>
+</html>
